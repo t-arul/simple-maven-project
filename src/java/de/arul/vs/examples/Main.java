0fa1a0f5bcb5bf1f690908772b4ab8665792c2b0
Tolga Arul <tolga@arul.de>
2019-10-02 11:40:21 +0200

    Adjustments

--- src/java/de/arul/vs/examples/Main.java
+++ src/java/de/arul/vs/examples/Main.java
@@ -1,19 +1,21 @@
-package de.dhbw.vs.examples;
+package de.arul.vs.examples;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.dhbw.vs.utils.logging.LogLevel;
-import de.dhbw.vs.utils.logging.Logging;
+import de.arul.vs.utils.logging.LogLevel;
+import de.arul.vs.utils.logging.Logging;
 
+public class Main
+{
 
-public class Main {
-
-	static {
+	static
+	{
 		Logging.setLoggingDefaults(LogLevel.DEBUG, "[%-5p; %c{1}::%M] %m%n");
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// Obtain an instance of a logger for this class
 		Logger log = LoggerFactory.getLogger(Main.class);
 
